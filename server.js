877fddb42d2c867c59bcd42d11c8817c8dbd08c5
Prathvi-P-Shetty <prathvipshetty23@gmail.com>
2026-01-01 14:11:43 +0530

    Update server to use dynamic PORT for deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import 'dotenv/config'
 
 //Creating Express App
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware that converts frontend JSON data to js object
 app.use(express.json());
